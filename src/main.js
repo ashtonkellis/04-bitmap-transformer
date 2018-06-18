5702ac3f5f9bd01c63f1e237b8b1b622891a931b
Ashton K Ellis <ashtonkellis@gmail.com>
2018-06-17 19:46:16 -0700

    fiddline with color table

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@
 
 // this is where we get the command line arguments. 
 // its an array, the user input arguments start at index position 2
-console.log(process.argv); // eslint-disable-line 
+// console.log(process.argv); // eslint-disable-line 
 
 const fs = require('fs');
 const Bitmap = require('./model/index');
@@ -13,8 +13,23 @@ fs.readFile(testImgPath, (err, data) => {
   if (err) {
     throw new Error('Read file is broken! oops!');
   }
-  console.log('data', data); // eslint-disable-line
-
   const newBitmap = new Bitmap(data);
-  console.log(newBitmap); // eslint-disable-line
+  // height should be 55,138 bytes
+  // width shoudl be 110
+  // height should be 125
+  console.log('dib header size:', newBitmap.dibHeaderSize);
+  console.log('    width:', newBitmap.width); // eslint-disable-line
+  console.log('   height:', newBitmap.height); // eslint-disable-line
+  console.log('color table:', newBitmap.colorTable);
 });
+
+
+// console.log(buf);
+// console.log(buf.toString());
+
+// arg is the number of bits to skip before starting to read
+// console.log(buf.readUInt8(0));
+
+// reading the first 10 bits
+// console.log(buf.readUInt16BE(5));
+// console.log(buf.readUInt32LE(5));
--- src/model/index.js
+++ src/model/index.js
@@ -8,19 +8,23 @@
 module.exports = class Bitmap { // eslint-disable-line
   constructor(buffer) {
     const fileSizeOffset = 2;
+
+    const dibHeaderSizeOffset = 14;
     const bitmapWidth = 18; 
-    const bitmapHeight = 20;
+    const bitmapHeight = 22;
     const colorTableLength = 1078; // eslint-disable-line
     // TODO: find which decimal number the colorTableOffset starts at
-    const colorTableOffset = null; // eslint-disable-line
-    // the readUInt32LE below should possibly/probably be be readUInt16 instead
-    this.fileSize = buffer.readUInt32LE(fileSizeOffset);
-    this.height = buffer.readUInt32LE(bitmapHeight);
-    this.width = buffer.readUInt32LE(bitmapWidth);
+    const colorTableOffset = 68;
+    this.dibHeaderSize = buffer.readUInt16LE(dibHeaderSizeOffset);
+    this.width = buffer.readUInt16LE(bitmapWidth);
+    this.height = buffer.readUInt16LE(bitmapHeight);
     // this.colorTable = some method from the Buffer class that passes in the 
     // colorTableOffset variable 
     // and the colorTableLength so you can just access that portion of the buffer 
     // at that offset and manipulate that data.
+    this.colorTable = buffer.slice(colorTableOffset, colorTableOffset + colorTableLength);
+    
+    
   }
 
   // possible methods
